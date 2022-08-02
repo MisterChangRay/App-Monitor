8b0ed6e1ff6a82b9b01194fdf08bcb3d7511db11
chang.ray <misterchangray@hotmail.com>
2022-08-02 17:37:33 +0800

    feat: add server_info ddl

--- app-monitor-server/src/main/resources/db/schema-h2.sql
+++ app-monitor-server/src/main/resources/db/schema-h2.sql
@@ -15,3 +15,14 @@ CREATE TABLE IF NOT EXISTS  app_info (
   auto_restart int DEFAULT 0,
   test_url varchar(255) DEFAULT NULL
 ) ;
+
+
+
+CREATE TABLE IF NOT EXISTS  server_info (
+  ip varchar(255) DEFAULT NULL ,
+  port varchar(255) DEFAULT NULL,
+  password varchar(255) DEFAULT NULL ,
+  username varchar(255) DEFAULT NULL,
+  remark varchar(255) DEFAULT NULL,
+
+) ;
